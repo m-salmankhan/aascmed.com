60a26b33e734200284c89ae15837f0b437cf4525
Salman Khan <salman@msalmankhan.co.uk>
2023-07-09 23:44:16 +0100

    make default opengraph/twitter image favicon

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -7,7 +7,6 @@ interface SEOProps {
     description: string
     slug: string
     title: string
-    language?: string
     image?: string
     children?: ReactNode
 }
@@ -27,16 +26,14 @@ export const SEO = (props: SEOProps) => {
     const siteTitle = siteMetadata.title;
     const siteURL = siteMetadata.url;
     const twitterHandle = siteMetadata.twitter;
-    const language = props.language || "en";
 
     const pageTitle = (props.title ? props.title + " | " : "") + siteTitle;
-    const socialImage = props.image ? siteURL + props.image : undefined;
+    const socialImage = props.image ? siteURL + props.image : `${siteURL}/assets/favicon/favicon-300x300.png`;
 
     return (
         <>
             <title>{pageTitle}</title>
             <meta name={"description"} content={props.description} />
-            <meta name={"language"} content={language} />
             <Favicons />
 
             {/* OpenGraph */}
@@ -44,24 +41,17 @@ export const SEO = (props: SEOProps) => {
             <meta name={"og:title"} content={pageTitle} />
             <meta name={"og:description"} content={props.description} />
             <meta name={"theme-color"} content={colours.brandPrimary} />
-            {
-                socialImage &&
-                <meta name={"og:image"} content={socialImage} />
-            }
+            <meta name={"og:image"} content={socialImage} />
 
             {/* Twitter Cards */}
             <meta name={"twitter:card"} content={"summary"} />
+            <meta name={"twitter:image"} content={socialImage} />
+            <meta name={"twitter:description"} content={props.description} />
             {
                 twitterHandle &&
                 <meta name={"twitter:site"} content={twitterHandle} />
             }
-            {
-                socialImage &&
-                <meta name={"twitter:image"} content={socialImage} />
-            }
-            <meta name={"twitter:description"} content={props.description} />
             {props.children}
         </>
     );
-
 }
