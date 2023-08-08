14071097b359fd40e4041c9721b00ce62ce38a90
Salman Khan <salman@msalmankhan.co.uk>
2023-08-08 13:38:20 +0100

    fix noscript static map

--- aascmed.com/src/components/mapbox/static.tsx
+++ aascmed.com/src/components/mapbox/static.tsx
@@ -33,6 +33,8 @@ const stylesStaticMap = css`
       left: 0;
       width: 100%;
       height: 100%;
+      object-fit: cover;
+      object-position: center;
       transition: opacity .5s ease 0s, filter .5s ease 0s;
       filter: blur(20px);
       display: block;
@@ -104,8 +106,11 @@ export const StaticMap: React.FC<MapBoxProps> = (props) => {
     return (
         <div className={props.className} css={css(stylesStaticMap)}>
             <noscript>
-                <Global styles={css`#${id} { display: none; }`} />
-                <img alt={props.alt} className={`map ${ImageState.COMPLETED}`} src={image.src} srcSet={image.srcset} sizes={image.sizes} />
+                <Global styles={css`#${CSS.escape(id)} { display: none; }`} />
+                <div className="lazy-container noscript">
+                    <img alt={props.alt} className={`map ${ImageState.COMPLETED}`} src={image.src} srcSet={image.srcset} sizes={image.sizes} />
+                </div>
+
             </noscript>
             <div id={id} className={`lazy-container`}>
                 {
