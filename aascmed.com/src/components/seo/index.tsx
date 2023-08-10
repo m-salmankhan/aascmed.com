17d1a27fa7ccbff8d16d45ca5e935b27b8fcb9b3
Salman Khan <salman@msalmankhan.co.uk>
2023-08-10 18:55:52 +0100

    add google search console and facebook pixel code

--- aascmed.com/src/components/seo/index.tsx
+++ aascmed.com/src/components/seo/index.tsx
@@ -37,8 +37,29 @@ export const SEO = (props: SEOProps) => {
 
     return (
         <>
+            <meta name="google-site-verification" content="AJxvzTXiJ6uf53Wmy2F6q-aoLvLGVCjDsHWfTU4Mhkg" />
             <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9XCDLT1NCL"></Script>
             <Script id={"gtag"} dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-9XCDLT1NCL');` }} />
+
+            {/* Facebook Pixel Code */}
+            <Script dangerouslySetInnerHTML={{
+                __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '856080292125355'); 
+fbq('track', 'PageView');`}} />
+            <noscript>
+                <img height="1" width="1"
+                    src="https://www.facebook.com/tr?id=856080292125355&ev=PageView
+&noscript=1"/>
+            </noscript>
+
+
             <html lang="en-US" />
             <title>{pageTitle}</title>
             <meta name={"description"} content={props.description} />
